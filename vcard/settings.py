f8b08f1d630fbcfaf5fa0744c0910072faece522
Gift <sokogfb@gmail.com>
2023-01-06 15:17:28 +0200

    Added Static Root

--- vcard/settings.py
+++ vcard/settings.py
@@ -115,10 +115,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  # 'data' is my media folder
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
